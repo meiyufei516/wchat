07393abf360f45f8e51f1ca4aca7f0414183c408
393431219@qq.com <WSMYF516myf>
2018-12-04 15:29:13 +0800

    修改单例双锁的注释

--- src/main/java/com/myf/wchat/web/test/singeltontest/SingelTon6.java
+++ src/main/java/com/myf/wchat/web/test/singeltontest/SingelTon6.java
@@ -4,9 +4,7 @@ package com.myf.wchat.web.test.singeltontest;
  * @author MeiYF
  * @time 2018/12/3 15:46
  * 枚举
- * 这种方式不仅能避免多线程同步问题，而且还能防止反序列化重新创建新的对象，
- * 可谓是很坚强的壁垒啊，不过，由于1.5中才加入enum特性，用这种方式写不免让人感觉生疏，
- * 在实际工作中，我也很少看见有人这么写过。
+ * 这种方式不仅能避免多线程同步问题，而且还能防止反序列化重新创建新的对象
  **/
 public class SingelTon6 {
 
@@ -16,10 +14,9 @@ public class SingelTon6 {
 		return SingelTon.INSTANCE.getsingelTon6();
 	}
 
+
 	private enum SingelTon {
-
 		INSTANCE;
-
 		private SingelTon6 singelTon6;
 
 		//JVM会保证此方法绝对只调用一次
@@ -32,6 +29,10 @@ public class SingelTon6 {
 		}
 	}
 
+	/**
+	 * main测试
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SingelTon6 s1=SingelTon6.getsingelTon6();
 		SingelTon6 s2=SingelTon6.getsingelTon6();
--- src/main/java/com/myf/wchat/web/test/singeltontest/SingleTon7.java
+++ src/main/java/com/myf/wchat/web/test/singeltontest/SingleTon7.java
@@ -4,6 +4,14 @@ package com.myf.wchat.web.test.singeltontest;
  * @author MeiYF
  * @time 2018/12/3 15:52
  * 双重校验锁
+ *     volatile关键字，考虑的是，new关键字在虚拟机中执行时其实分为很多步骤，
+ * 具体原因可以参考深入理解java虚拟机一书（考虑的是这个new关键字字节码执行时是非原子性的），
+ * 而volatile关键字可以防止指令重排。
+ *
+ *     假如有A、B、C三个线程，A、B比C快，A和B都通过了0处到达1处，判断了对象为null后，
+ * A和B到达2处，A进入锁，到达3处再进行一次null判断，如果是null,就创建对象。
+ *     当A运行完后释放锁，B进入锁到达3处，判断此时的对象不为空，就直接返回对象。
+ *     此时C通过0处到达1处，判断对象不为Null,就直接返回对象。
  **/
 public class SingleTon7 {
 
@@ -11,10 +19,10 @@ public class SingleTon7 {
 
 	private volatile static SingleTon7 singleTon7;
 
-	public static SingleTon7 getSingleTon7(){
-		if (singleTon7 == null) {
-			synchronized (SingleTon7.class) {
-				if (singleTon7 == null){
+	public static SingleTon7 getSingleTon7(){//0
+		if (singleTon7 == null) {//1
+			synchronized (SingleTon7.class) {//2
+				if (singleTon7 == null){//3
 					singleTon7 = new SingleTon7();
 				}
 			}
